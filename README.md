880826af63c0c23e396175584e5bf0c0d450a054
Fausto28 <festamatti28@gmail.com>
2024-01-16 11:59:51 -0300

    Cambio de repositorio

---
+++ README.md
@@ -0,0 +1,14 @@
+# Preentrega 3 Comisión 56060
+
+## Nombre y Apellido
+- Fausto Estamatti.
+
+## Resumen del proyecto
+
+- Se trata de una página de renta de coches para América del Sur donde se lleva un registro del cliente con nacionalidad y el auto que alquiló.
+
+## Pasos para ejecutar el proyecto.
+
+- Link de GitHub: https://github.com/Fausto28/MiPrimerProyecto-FaustoEstamatti.git
+- Creación del entorno virtual 
+- Instalar las dependencias con pip requirements
---
+++ project/cliente/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Auto(models.Model):
+    marca=models.CharField(max_length=50)
+    modelo=models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f"{self.marca}, {self.modelo}"
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name = "país"
+        verbose_name_plural = "países"
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen_id = models.ForeignKey(
+        Pais, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="país de origen"
+    )
+    automovil=models.ForeignKey(Auto, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='auto')
+
+    def __str__(self) -> str:
+        return f"{self.apellido}, {self.nombre}"
+    
+
