41d02b2bade167c39f4500bd7d963d87d0a77227
Fausto28 <festamatti28@gmail.com>
2024-01-17 11:16:43 -0300

    Primer parte clase 12

--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from django.urls import reverse
 
 def index(request):
-    return render(request, "core/index.html")
+    contexto={'index': False, 'about':False, 'clientes':False}
+    if request.path == reverse('core:index'):
+        contexto['index']= True
+    elif request.path == reverse('core:about'):
+        contexto['about']= True
+        
+    elif request.path == reverse('core:clientes'):
+        contexto['clientes']= True
+        
+    return render(request, "core/index.html",contexto)
+
+def about(request):
+    
+    return render(request, "core/about.html")
